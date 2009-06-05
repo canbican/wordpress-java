fe453931b09577d333f1e5d035db2fa20f584971
can.bican <can.bican@e28c6f1b-1445-0410-b4e7-ad7e8fe3e338>
2009-06-05 08:33:04 +0000

    minor change, added serial id for the exception.
    git-svn-id: https://wordpress-java.googlecode.com/svn/trunk@40 e28c6f1b-1445-0410-b4e7-ad7e8fe3e338

--- src/net/bican/wordpress/InvalidPostFormatException.java
+++ src/net/bican/wordpress/InvalidPostFormatException.java
@@ -21,8 +21,11 @@ package net.bican.wordpress;
  * Exception that is triggered via invalid input
  * 
  * @author Can Bican &lt;can@bican.net&gt;
- *
+ * 
  */
 public class InvalidPostFormatException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
   // just a default exception
 }
