804cd8d19a432a32a6157b313dedc6f85f13fb8c
can.bican <can.bican@e28c6f1b-1445-0410-b4e7-ad7e8fe3e338>
2010-04-04 21:36:26 +0000

    Formatting and copyright header on the new file.
    git-svn-id: https://wordpress-java.googlecode.com/svn/trunk@57 e28c6f1b-1445-0410-b4e7-ad7e8fe3e338

--- src/net/bican/wordpress/CustomField.java
+++ src/net/bican/wordpress/CustomField.java
@@ -1,11 +1,26 @@
+/*
+ * This file is part of jwordpress.
+ *
+ * jwordpress is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jwordpress is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jwordpress.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.bican.wordpress;
-
 
 /**
  * This class represents custom fields in wordpress.
  * 
  * @author alok
- *
+ * 
  */
 public class CustomField extends XmlRpcMapped implements StringHeader {
   String id = null;
@@ -13,57 +28,57 @@ public class CustomField extends XmlRpcMapped implements StringHeader {
   String key = null;
 
   String value = null;
-  
- /**
-  * (non-Javadoc)
-  * 
-  * @see net.bican.wordpress.StringHeader#getStringHeader()
-  */
- public String getStringHeader() {
-   final String TAB = ":";
-   return "Id" + TAB + "Key" + TAB + "Value";
- }
 
-/**
- * @return id of custom field
- */
-public String getId() {
-  return id;
-}
+  /**
+   * (non-Javadoc)
+   * 
+   * @see net.bican.wordpress.StringHeader#getStringHeader()
+   */
+  public String getStringHeader() {
+    final String TAB = ":";
+    return "Id" + TAB + "Key" + TAB + "Value";
+  }
 
-/**
- * @param id
- */
-public void setId(String id) {
-  this.id = id;
-}
+  /**
+   * @return id of custom field
+   */
+  public String getId() {
+    return id;
+  }
 
-/**
- * @return key of custom field
- */
-public String getKey() {
-  return key;
-}
+  /**
+   * @param id
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
 
-/**
- * @param key
- */
-public void setKey(String key) {
-  this.key = key;
-}
+  /**
+   * @return key of custom field
+   */
+  public String getKey() {
+    return key;
+  }
 
-/**
- * @return value of custom field
- */
-public String getValue() {
-  return value;
-}
+  /**
+   * @param key
+   */
+  public void setKey(String key) {
+    this.key = key;
+  }
 
-/**
- * @param value
- */
-public void setValue(String value) {
-  this.value = value;
-}
-  
+  /**
+   * @return value of custom field
+   */
+  public String getValue() {
+    return value;
+  }
+
+  /**
+   * @param value
+   */
+  public void setValue(String value) {
+    this.value = value;
+  }
+
 }
