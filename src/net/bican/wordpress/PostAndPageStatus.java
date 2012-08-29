9731e2df84be0f05193f214f914c5767167a04e9
can@bican.net <can@bican.net@e28c6f1b-1445-0410-b4e7-ad7e8fe3e338>
2012-08-29 07:54:20 +0000

    Changed licensing from GPLV3 to GPLV3+MIT.
    git-svn-id: https://wordpress-java.googlecode.com/svn/trunk@96 e28c6f1b-1445-0410-b4e7-ad7e8fe3e338

--- src/net/bican/wordpress/PostAndPageStatus.java
+++ src/net/bican/wordpress/PostAndPageStatus.java
@@ -1,18 +1,11 @@
 /*
- * This file is part of jwordpress.
- *
- * jwordpress is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * jwordpress is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with jwordpress.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Wordpress-java
+ * http://code.google.com/p/wordpress-java/
+ * 
+ * Copyright 2012 Can Bican <can@bican.net>
+ * See the file 'COPYING' in the distribution for licensing terms.
+ * 
  */
 package net.bican.wordpress;
 
